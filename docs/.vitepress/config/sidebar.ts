5e56133e098445199a7727ab453a4e123f8314ba
xuanxiaoqian <3102535247@qq.com~>
2026-06-17 23:58:06 +0800

    新增文字

--- docs/.vitepress/config/sidebar.ts
+++ docs/.vitepress/config/sidebar.ts
@@ -7,6 +7,14 @@ export const sidebarConfig: DefaultTheme.Sidebar = {
         {
           text: "盯盘程序",
           link: "/pages/交易/1-盯盘程序"
+        },
+        {
+          text: "理财",
+          link: "/pages/交易/2-理财"
+        },
+        {
+          text: "交易本质",
+          link: "/pages/交易/3-交易本质"
         }
       ]
     }
