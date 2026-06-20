2defcb066ae537828c6a7334996d1ca1868e2130
xuanxiaoqian <3102535247@qq.com~>
2026-06-20 15:30:20 +0800

    2026年6月20日15:30:19

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -3,7 +3,17 @@ import { DefaultTheme } from 'vitepress'
 export const navConfig: DefaultTheme.NavItem[] = [
   {
     text: "交易",
-    link: "/pages/交易/1-盯盘程序",
-    activeMatch: "/pages/交易/"
+    items: [
+      {
+        text: "心得",
+        link: "/pages/交易/心得/1-盯盘程序",
+        activeMatch: "/pages/交易/心得/"
+      },
+      {
+        text: "系统",
+        link: "/pages/交易/系统/1-总览",
+        activeMatch: "/pages/交易/系统/"
+      }
+    ]
   }
 ]
--- docs/.vitepress/config/sidebar.ts
+++ docs/.vitepress/config/sidebar.ts
@@ -1,20 +1,30 @@
 import { DefaultTheme } from 'vitepress'
 
 export const sidebarConfig: DefaultTheme.Sidebar = {
-  ['/pages/交易/']: [
+  ['/pages/交易/心得/']: [
     {
       items: [
         {
           text: "盯盘程序",
-          link: "/pages/交易/1-盯盘程序"
+          link: "/pages/交易/心得/1-盯盘程序"
         },
         {
           text: "理财",
-          link: "/pages/交易/2-理财"
+          link: "/pages/交易/心得/2-理财"
         },
         {
           text: "交易本质",
-          link: "/pages/交易/3-交易本质"
+          link: "/pages/交易/心得/3-交易本质"
+        }
+      ]
+    }
+  ],
+  ['/pages/交易/系统/']: [
+    {
+      items: [
+        {
+          text: "总览",
+          link: "/pages/交易/系统/1-总览"
         }
       ]
     }
